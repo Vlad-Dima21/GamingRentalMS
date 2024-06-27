8641954a0c0248979e6a1369dd8f2a1b1872123e
Vlad-Dima21 <vladima2001@gmail.com>
2024-06-27 18:39:10 +0300

    open api config minor fix

--- services/product/src/main/java/com/vladima/gamingrental/configs/OpenAPIConfig.java
+++ services/product/src/main/java/com/vladima/gamingrental/configs/OpenAPIConfig.java
@@ -2,7 +2,6 @@ package com.vladima.gamingrental.configs;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
